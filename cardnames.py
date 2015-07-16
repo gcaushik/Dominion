6887324f00de5ebd937f59eb9f7d9f24df29e417
Gokul <caushik1@gmail.com>
2015-07-16 16:42:43 -0700

    Created cardnames.py

---
+++ cardnames.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+  
+curse = 0
+estate = 1
+duchy = 2
+province = 3
+copper = 4
+silver = 5
+gold = 6
+adventurer = 7
+ambassador = 8
+baron = 9
+council_room = 10
+cutpurse = 11
+embargo = 12
+feast = 13
+gardens = 14
+great_hall = 15
+mine = 16
+outpost = 17
+smithy = 18
+village = 19
+
+curse_cards = [curse]
+victory_cards = [estate, duchy, province]
+treasure_cards = [copper, silver, gold]
+victory_kingdom_cards = [gardens, great_hall]
+kingdom_cards = [adventurer, ambassador, baron, council_room, cutpurse, embargo, feast, mine, outpost, smithy, village]
